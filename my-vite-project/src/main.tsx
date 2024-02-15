bf1637edacc2c33df6f91391fc93c2f3e4b64296
Aarushyada <Aarush@kenresearch.com>
2024-02-15 11:18:27 +0530

    second commit

--- my-vite-project/src/main.tsx
+++ my-vite-project/src/main.tsx
@@ -1,3 +1,47 @@
+// // seo implementation 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import NotFoundPage from './pages/NotFoundPage';
+// import ProfilePage from './pages/ProfilePage';
+// import ProfilesPage from './pages/ProfilesPage';
+// import { Helmet, HelmetProvider } from 'react-helmet';
+
+// import './index.css';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//     errorElement: <NotFoundPage />,
+//   },
+//   {
+//     path: '/profiles',
+//     element: <ProfilesPage />,
+//     children: [
+//       {
+//         path: '/profiles/:profileId',
+//         element: <ProfilePage />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <HelmetProvider>
+//       <RouterProvider router={router} />
+//       <Helmet>
+//         <title>REPORT PAGE</title>
+//         {/*  metadata */}
+//       </Helmet>
+//     </HelmetProvider>
+//   </React.StrictMode>,
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -31,4 +75,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
+
 
